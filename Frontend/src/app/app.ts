ee8cc6804de2391e50a5fc0fffa659d017acdb08
AlexVilchez-Inetum <alexandre.vilchez.ext@inetum.com>
2025-09-22 12:04:04 +0200

    Install PrimeNG, modify `app.config.ts` and testing

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { Ripple } from 'primeng/ripple';
+import { Toast } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, Toast, Ripple],
+  providers: [MessageService],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('Frontend');
+
+  constructor(private messageService: MessageService) {}
+
+    showSuccess() {
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'UP2Play application is running!' });
+    }
 }
