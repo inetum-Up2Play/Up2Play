b3d22297a341c2708c3492cc97941ec8c12d5ff3
davi2006-sudo <dasava1979@gmail.com>
2025-10-02 12:47:46 +0200

    feat: Añadir dependencia validacion + @Email @NotBlank en el campo email y SEQUENCE en la ID

--- Backend_Up2Play/backend/src/main/java/com/Up2Play/backend/Model/UsuarioModel.java
+++ Backend_Up2Play/backend/src/main/java/com/Up2Play/backend/Model/UsuarioModel.java
@@ -1,18 +1,25 @@
 package com.Up2Play.backend.Model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "USUARIO")
 public class UsuarioModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(unique = true, nullable = false)
     private Long id;
+
+    @NotBlank
+    @Email
     private String email;
     private String contraseña;
     private String rol;
