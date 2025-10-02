b9e32083aa29b0b98dfd980f2b80e4514c0ad99e
Alex-Vilchez <alexvilchezpena@gmail.com>
2025-10-02 10:34:38 +0200

    feat: crear en frontend feature/auth/pages para "login" y "register" y modificar app.routes.ts

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/pages/login/login';
+import { Register } from './features/auth/pages/register/register';
+import { Home } from './features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'auth/login',
+        component: Login
+    },
+    {
+        path: 'auth/register',
+        component: Register
+    }
+];
