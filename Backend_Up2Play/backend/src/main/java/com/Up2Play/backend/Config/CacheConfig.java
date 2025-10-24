6d152cec4b1cbc5b2dd5539e3b7dd546e9783329
africa-rmico <africa.rmico@gmail.com>
2025-10-24 14:20:46 +0200

    feat: limitar acceso al iniciar sesión

---
+++ Backend_Up2Play/backend/src/main/java/com/Up2Play/backend/Config/CacheConfig.java
@@ -0,0 +1,36 @@
+package com.Up2Play.backend.Config;
+
+import java.util.concurrent.TimeUnit;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+//configuración que controla el comportamiento el caché, como la expiración o los limites del caché.
+
+@Configuration
+@EnableCaching //activa el soporte de caché en Spring. Esto permite usar anotaciones como @Cacheable, @CachePut, @CacheEvict.
+public class CacheConfig {
+
+    //Se define  y configura el objeto "Caffeine" que se utilizará en el metodo de abajo
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder() //crea una configuración personalizada para caffeine
+        .expireAfterWrite(10, TimeUnit.MINUTES)
+        .maximumSize(1000);
+    }
+
+    //Este método define el gestor de caché principal, donde recoge el objeto Caffeine de arriba
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager("loginAttempts");
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+
+}
+
