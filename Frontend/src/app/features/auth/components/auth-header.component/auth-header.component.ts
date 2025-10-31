e1b16482fbd1b8895ff5b6798ab498cced3bb204
JoJu2 <joanjuncosavilaplana@gmail.com>
2025-10-31 14:22:02 +0100

    feat: menú navbar en movil

--- Frontend/src/app/features/auth/components/auth-header.component/auth-header.component.ts
+++ Frontend/src/app/features/auth/components/auth-header.component/auth-header.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { Menu, MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-auth-header',
-  imports: [RouterModule],
+  imports: [RouterModule, Menu, MenuModule, ButtonModule],
   templateUrl: './auth-header.component.html',
   styleUrl: './auth-header.component.scss'
 })
@@ -17,6 +20,20 @@ export class AuthHeaderComponent {
   constructor(private router: Router) {
   }
 
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'MENÚ',
+        items: [
+          { label: 'Inicio', routerLink: ['http://127.0.0.1:5500/Up2Play/Landing/index.html'] }, // Angular Router
+          { label: 'Registrarse', routerLink: ['/auth/register'] }, // Angular Router
+          { label: 'Iniciar sesión', routerLink: ['/auth/login'] }, // Angular Router
+        ]
+      }
+    ];
+  }
 
   isLogin(): boolean {
     return this.router.url === this.IniciarSesionUrl;
