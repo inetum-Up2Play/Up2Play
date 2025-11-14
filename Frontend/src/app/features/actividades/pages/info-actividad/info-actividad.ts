55a1be53c7582c463e88f34e19c7853999a88e37
Alex-Vilchez <alexvilchezpena@gmail.com>
2025-11-14 12:58:50 +0100

    feat:interfaz info actividad

--- Frontend/src/app/features/actividades/pages/info-actividad/info-actividad.ts
+++ Frontend/src/app/features/actividades/pages/info-actividad/info-actividad.ts
@@ -1,17 +1,29 @@
 import { Component, input, signal } from '@angular/core';
 
 import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { Rating } from 'primeng/rating';
+import { InputIcon } from 'primeng/inputicon';
+
 import { Actividad } from '../../../../core/models/Actividad';
 import { ActService } from '../../../../core/services/actividad/act-service';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-info-actividad',
-  imports: [CardModule],
+  imports: [CardModule, DividerModule, Rating, InputIcon, FormsModule, ReactiveFormsModule],
   templateUrl: './info-actividad.html',
   styleUrl: './info-actividad.scss'
 })
 export class InfoActividad {
   actividad = signal<Actividad | null>(null);
+
+  //Todavía no funciona bien, es para el nivel de actividad
+  nivelControl = new FormControl(3);
+  form = new FormGroup({
+    rating: new FormControl(0)
+  });
+
 
   constructor(private actService: ActService) {
     // Simulación: obtener actividad por ID
@@ -19,4 +31,22 @@ export class InfoActividad {
       this.actividad.set(data); // Actualizamos la signal con la respuesta
     });
   }
+
+  //Método para indicar las banderas según nivel
+  getNivelValue(nivel: string): number {
+    const map: Record<string, number> = {
+      'Iniciado': 1,
+      'Principiante': 2,
+      'Intermedio': 3,
+      'Avanzado': 4,
+      'Experto': 5
+    };
+    return map[nivel] || 0; // Devuelve 0 si no coincide
+  }
+
+
+  // Creo que aquí debería ir la lógica para según el deporte que sea, 
+  // darle un valor al actividad.imagen distinto y así se muestre luego
+  // switch (this.actividad.deporte) {
+  // }
 }
