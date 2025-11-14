e238a43ec501c697b06f279ffe39ad8f0fd0556e
MarcBalague <marcbalague2004@gmail.com>
2025-11-14 11:23:02 +0100

    feat: HTML, CSS, lógica de HTML terminada

--- Frontend/src/app/core/layout/header/header.ts
+++ Frontend/src/app/core/layout/header/header.ts
@@ -1,5 +1,11 @@
 import { Router, RouterModule } from '@angular/router';
-import { Component, inject, signal } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenubarModule } from 'primeng/menubar';
 import { RippleModule } from 'primeng/ripple';
@@ -11,11 +17,9 @@ import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
 import { MenuItem } from 'primeng/api';
 
-
-
 interface MenuItemPages {
   label: string;
-  icon: string;     
+  icon: string;
   route?: string;
   children?: MenuItem[];
 }
@@ -23,9 +27,7 @@ interface MenuItemPages {
 interface AvatarItem {
   label: string;
   icon: string;
-
 }
-
 
 @Component({
   selector: 'app-header',
@@ -37,39 +39,39 @@ interface AvatarItem {
     AvatarModule,
     InputTextModule,
     DrawerModule,
-    ButtonModule,    
+    ButtonModule,
     MenuModule,
   ],
   templateUrl: './header.html',
-  styleUrls: ['./header.scss']
+  styleUrls: ['./header.scss'],
 })
 export class Header {
-  
   private router = inject(Router);
-
-  landingUrl = 'http://127.0.0.1:5500/Up2Play/Landing/index.html'
-
   visible = false;
 
   items: MenuItemPages[] = [
     { label: 'Inicio', icon: 'pi pi-home', route: '/' },
-    { label: 'Actividades', icon: 'pi pi-bookmark', route: '/actividades'},
-    { label: 'Pagos', icon: 'pi pi-chart-line', route: '/pagos'},
+    { label: 'Actividades', icon: 'pi pi-bookmark', route: '/actividades' },
+    { label: 'Pagos', icon: 'pi pi-chart-line', route: '/pagos' },
     { label: 'Notificaciones', icon: 'pi pi-users', route: '/notificaciones' },
     { label: 'Historial', icon: 'pi pi-calendar', route: '/historial' },
     { label: 'Mi Cuenta', icon: 'pi pi-cog', route: '/micuenta' },
   ];
-  
+
   trackByLabel(index: number, item: MenuItemPages) {
     return item.label;
+  }
+
+  // Página activa 
+  isActive(route: string, exact = true): boolean {
+    return exact ? this.router.url === route : this.router.url.startsWith(route);
   }
 
 
   avatarItems: MenuItem[] = [];
   userEmail = 'usuario@tu-dominio.com';
 
-  
- ngOnInit(): void {
+  ngOnInit(): void {
     this.avatarItems = [
       {
         label: this.userEmail,
@@ -78,8 +80,8 @@ export class Header {
       {
         label: 'Cerrar sesión',
         icon: 'pi pi-sign-out',
-        command: () => this.logout()
-      }
+        command: () => this.logout(),
+      },
     ];
   }
 
@@ -89,9 +91,34 @@ export class Header {
     console.log('Cerrar sesión');
   }
 
+  // Calcular tamaño del header
+  //Busca id menu-superior en el html, con ElementRef lo referencia
+  @ViewChild('menu-superior', { static: true }) headerRef!: ElementRef<HTMLElement>;
 
+  //ResizeObserver para observar cambios en el tamaño del header
+  private ro?: ResizeObserver;
 
+  //Después de que la vista se haya inicializado
+  ngAfterViewInit() {
+    const headerEl = this.headerRef.nativeElement;
 
+    // Calculo altura del header y la asigno a la variable CSS --header-h
+    const setVar = () => {
+      const h = headerEl.getBoundingClientRect().height;
+      document.documentElement.style.setProperty('--header-h', `${h}px`);
+    };
 
+    // Inicializo la variable CSS --header-h
+    setVar();
 
+    // Observa cambios dinámicos
+    this.ro = new ResizeObserver(setVar);
+    this.ro.observe(headerEl);
+  }
+
+  // Se elimina el recurso si sales del componente(ej login)
+  ngOnDestroy() {
+    this.ro?.disconnect();
+  }
+    
 }
