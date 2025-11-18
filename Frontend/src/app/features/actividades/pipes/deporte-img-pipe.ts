91cda77a1e3de495cb5e9e6d292056b71aa7ad9d
Alex-Vilchez <alexvilchezpena@gmail.com>
2025-11-18 10:35:13 +0100

    feat: listar deportes en pipe-deportes-img

--- Frontend/src/app/features/actividades/pipes/deporte-img-pipe.ts
+++ Frontend/src/app/features/actividades/pipes/deporte-img-pipe.ts
@@ -5,16 +5,63 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DeporteImgPipe implements PipeTransform {
 
-  transform(deporte: string): string {
+  
+transform(deporte: string): string {
     const cdnBase = 'https://cdn.tu-servidor.com/deportes/';
     const map: Record<string, string> = {
-      'Running': 'running.jpg',
-      'Fútbol': 'futbol.jpg',
-      'Baloncesto': 'baloncesto.jpg',
-      'Tenis': 'tenis.jpg',
+      'Arquería': 'arqueria.jpg',
+      'Atletismo': 'atletismo.jpg',
+      'Balonmano': 'balonmano.jpg',
+      'Basquet': 'basquet.jpg',
+      'Béisbol': 'beisbol.jpg',
+      'Billar': 'billar.jpg',
+      'Boxeo': 'boxeo.jpg',
+      'Canotaje': 'canotaje.jpg',
+      'Críquet': 'criquet.jpg',
+      'Ciclismo': 'ciclismo.jpg',
+      'Escalada': 'escalada.jpg',
+      'Esgrima': 'esgrima.jpg',
+      'Esquí': 'esqui.jpg',
+      'Futbol': 'futbol.jpg',
+      'Gimnasia': 'gimnasia.jpg',
+      'Golf': 'golf.jpg',
+      'Hockey': 'hockey.jpg',
+      'Judo': 'judo.jpg',
+      'Karate': 'karate.jpg',
       'Natación': 'natacion.jpg',
-      // ... añade las 20 opciones
+      'Patinaje': 'patinaje.jpg',
+      'Ping Pong': 'pingpong.jpg',
+      'Rugby': 'rugby.jpg',
+      'Remo': 'remo.jpg',
+      'Snowboard': 'snowboard.jpg',
+      'Softbol': 'softbol.jpg',
+      'Surf': 'surf.jpg',
+      'Taekwondo': 'taekwondo.jpg',
+      'Tenis': 'tenis.jpg',
+      'Triatlón': 'triatlon.jpg',
+      'Voleibol': 'voleibol.jpg',
+      'Waterpolo': 'waterpolo.jpg',
+      'Ajedrez': 'ajedrez.jpg',
+      'Badminton': 'badminton.jpg',
+      'Boxeo Tailandés': 'boxeo-tailandes.jpg',
+      'Capoeira': 'capoeira.jpg',
+      'Crossfit': 'crossfit.jpg',
+      'Danza Deportiva': 'danza-deportiva.jpg',
+      'Equitación': 'equitacion.jpg',
+      'Fútbol Americano': 'futbol-americano.jpg',
+      'Kickboxing': 'kickboxing.jpg',
+      'Lucha Libre': 'lucha-libre.jpg',
+      'Motocross': 'motocross.jpg',
+      'Padel': 'padel.jpg',
+      'Parkour': 'parkour.jpg',
+      'Pesas': 'pesas.jpg',
+      'Raquetbol': 'raquetbol.jpg',
+      'Skateboarding': 'skateboarding.jpg',
+      'Squash': 'squash.jpg',
+      'Tiro con Arco': 'tiro-con-arco.jpg',
+      'Ultimate Frisbee': 'ultimate-frisbee.jpg'
     };
+
     return cdnBase + (map[deporte] || 'default.jpg');
   }
 
