1e9bf6b8a1e1eda55fc6f28c5d9f45dfc754af6f
davi2006-sudo <dasava1979@gmail.com>
2025-11-19 12:49:18 +0100

    feat: Crear metodo para listar las actividades a las que el usuario no esta apuntado feat: Crear controlador de listar la actividades de las que el usuario no esta apuntado

--- Backend_Up2Play/backend/src/main/java/com/Up2Play/backend/Service/ActividadService.java
+++ Backend_Up2Play/backend/src/main/java/com/Up2Play/backend/Service/ActividadService.java
@@ -68,11 +68,10 @@ public class ActividadService {
         act.setEstado(EstadoActividad.fromValue("Pendiente"));
         act.setUsuarioCreador(usuario);
 
-        Usuario usuarioApuntado = usuarioRepository.findById(usuario.getId()).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        Usuario usuarioApuntado = usuarioRepository.findById(usuario.getId())
+                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
         act.getUsuarios().add(usuario);
         usuarioApuntado.getActividadesUnidas().add(act);
-
-
 
         Actividad actGuardada = actividadRepository.save(act);
 
@@ -81,7 +80,7 @@ public class ActividadService {
 
     // Listado todas las actividades
 
-    @Transactional //(readOnly = true)
+    @Transactional // (readOnly = true)
     public List<ActividadDtoResp> getAllActividades() {
         return actividadRepository.findAll().stream()
                 .map(a -> new ActividadDtoResp(
@@ -102,31 +101,31 @@ public class ActividadService {
     }
 
     // Lista de actividades creadas por un usuario
-    @Transactional //(readOnly = true)
+    @Transactional // (readOnly = true)
     public List<ActividadDtoCreadas> getActividadesCreadas(Usuario usuario) {
         return actividadRepository.findByUsuarioCreador(usuario).stream().map(a -> new ActividadDtoCreadas(
-                        a.getId(),
-                        a.getNombre(),
-                        a.getDescripcion(),
-                        a.getFecha() != null ? a.getFecha().toString() : null,
-                        a.getUbicacion(),
-                        a.getDeporte(),
-                        a.getNivel() != null ? a.getNivel().name() : null,
-                        a.getNum_pers_inscritas(),
-                        a.getNum_pers_totales(),
-                        a.getEstado() != null ? a.getEstado().name() : null,
-                        a.getPrecio(),
-                        a.getUsuarioCreador() != null ? a.getUsuarioCreador().getId() : null,
-                        a.getUsuarioCreador() != null ? a.getUsuarioCreador().getEmail() : null)).toList();
+                a.getId(),
+                a.getNombre(),
+                a.getDescripcion(),
+                a.getFecha() != null ? a.getFecha().toString() : null,
+                a.getUbicacion(),
+                a.getDeporte(),
+                a.getNivel() != null ? a.getNivel().name() : null,
+                a.getNum_pers_inscritas(),
+                a.getNum_pers_totales(),
+                a.getEstado() != null ? a.getEstado().name() : null,
+                a.getPrecio(),
+                a.getUsuarioCreador() != null ? a.getUsuarioCreador().getId() : null,
+                a.getUsuarioCreador() != null ? a.getUsuarioCreador().getEmail() : null)).toList();
     }
 
     // Lista de actividades a las que un usuario está apuntado
     @Transactional
     public List<ActividadDtoResp> getActividadesApuntadas(Long usuarioId) {
         Usuario usuario = usuarioRepository.findById(usuarioId)
-        .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
-    return usuario.getActividadesUnidas().stream()
-            .map(a -> new ActividadDtoResp(
+                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+        return usuario.getActividadesUnidas().stream()
+                .map(a -> new ActividadDtoResp(
                         a.getId(),
                         a.getNombre(),
                         a.getDescripcion(),
@@ -140,8 +139,34 @@ public class ActividadService {
                         a.getPrecio(),
                         a.getUsuarioCreador() != null ? a.getUsuarioCreador().getId() : null,
                         a.getUsuarioCreador() != null ? a.getUsuarioCreador().getEmail() : null))
-            .toList();
+                .toList();
 
+    }
+
+    //Lista de actividades a las que un usuario no esta apuntado
+    @Transactional
+    public List<ActividadDtoResp> getActividadesNoApuntadas(Long usuarioId) {
+
+        Usuario usuario = usuarioRepository.findById(usuarioId)
+                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+
+        return actividadRepository.findAll().stream()
+                .filter(act -> !usuario.getActividadesUnidas().contains(act))
+                .map(a -> new ActividadDtoResp(
+                        a.getId(),
+                        a.getNombre(),
+                        a.getDescripcion(),
+                        a.getFecha() != null ? a.getFecha().toString() : null,
+                        a.getUbicacion(),
+                        a.getDeporte(),
+                        a.getNivel() != null ? a.getNivel().name() : null,
+                        a.getNum_pers_inscritas(),
+                        a.getNum_pers_totales(),
+                        a.getEstado() != null ? a.getEstado().name() : null,
+                        a.getPrecio(),
+                        a.getUsuarioCreador() != null ? a.getUsuarioCreador().getId() : null,
+                        a.getUsuarioCreador() != null ? a.getUsuarioCreador().getEmail() : null))
+                .toList();
     }
 
     // Lista actividad por id
@@ -167,11 +192,12 @@ public class ActividadService {
 
     // Editar actividad
     public Actividad editarActividad(Long id, EditarActividadDto input) {
-        Actividad act = actividadRepository.findById(id).orElseThrow(() -> new ActividadNoEncontrada("Actividad no encontrada"));
+        Actividad act = actividadRepository.findById(id)
+                .orElseThrow(() -> new ActividadNoEncontrada("Actividad no encontrada"));
         act.setNombre(input.getNombre());
         act.setDescripcion(input.getDescripcion());
 
-       LocalDateTime fecha = LocalDateTime.parse(input.getFecha());
+        LocalDateTime fecha = LocalDateTime.parse(input.getFecha());
         if (fecha.isBefore(LocalDateTime.now())) {
             throw new FechaYHora("La fecha y hora no pueden ser anteriores al momento actual.");
         } else {
@@ -180,7 +206,6 @@ public class ActividadService {
 
         act.setUbicacion(input.getUbicacion());
         act.setNivel(NivelDificultad.fromValue(input.getNivel()));
-
 
         int num_personas_totales = Integer.parseInt(input.getNum_pers_totales());
 
@@ -195,32 +220,33 @@ public class ActividadService {
         return actEditada;
 
     }
+
     // Eliminar actividad
-    public void deleteActividad(Long id){
+    public void deleteActividad(Long id) {
         actividadRepository.deleteById(id);
     }
 
-    //Unirse a Actividad
-    public Actividad unirActividad (Long idActividad , Long idUsuario){
+    // Unirse a Actividad
+    public Actividad unirActividad(Long idActividad, Long idUsuario) {
         Actividad act = actividadRepository.findById(idActividad)
-            .orElseThrow(() -> new ActividadNoEncontrada("Actividad no encontrada"));
-        
+                .orElseThrow(() -> new ActividadNoEncontrada("Actividad no encontrada"));
+
         Usuario usuario = usuarioRepository.findById(idUsuario)
-            .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
-        
+                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+
         act.getUsuarios().add(usuario);
         usuario.getActividadesUnidas().add(act);
         return actividadRepository.save(act);
     }
 
-    //Desapuntarse a Actividad
-    public Actividad desapuntarActividad (Long idActividad , Long idUsuario){
+    // Desapuntarse a Actividad
+    public Actividad desapuntarActividad(Long idActividad, Long idUsuario) {
         Actividad act = actividadRepository.findById(idActividad)
-            .orElseThrow(() -> new ActividadNoEncontrada("Actividad no encontrada"));
-        
+                .orElseThrow(() -> new ActividadNoEncontrada("Actividad no encontrada"));
+
         Usuario usuario = usuarioRepository.findById(idUsuario)
-            .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
-        
+                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+
         act.getUsuarios().remove(usuario);
         usuario.getActividadesUnidas().remove(act);
         return actividadRepository.save(act);
