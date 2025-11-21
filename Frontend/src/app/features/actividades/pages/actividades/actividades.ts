cf8217355a428f281a9cd7f146b5512ddf0ca039
JoJu2 <joanjuncosavilaplana@gmail.com>
2025-11-21 14:09:25 +0100

    feat: Botón nueva actividad

--- Frontend/src/app/features/actividades/pages/actividades/actividades.ts
+++ Frontend/src/app/features/actividades/pages/actividades/actividades.ts
@@ -3,10 +3,11 @@ import { JoinGallery } from '../../components/join-gallery/join-gallery';
 import { Header } from '../../../../core/layout/header/header';
 import { JoinedCarousel } from '../../components/joined-carousel/joined-carousel';
 import { CreatedCarousel } from "../../components/created-carousel/created-carousel";
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-actividades',
-  imports: [JoinGallery, Header, JoinedCarousel, CreatedCarousel],
+  imports: [JoinGallery, Header, JoinedCarousel, CreatedCarousel, ButtonModule],
   templateUrl: './actividades.html',
   styleUrl: './actividades.scss'
 })
