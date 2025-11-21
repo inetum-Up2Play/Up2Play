f703804f1677bc832add2dd1349679f63070f735
JoJu2 <joanjuncosavilaplana@gmail.com>
2025-11-21 14:28:07 +0100

    feat: link botón crear actividad

--- Frontend/src/app/features/actividades/pages/actividades/actividades.ts
+++ Frontend/src/app/features/actividades/pages/actividades/actividades.ts
@@ -4,10 +4,11 @@ import { Header } from '../../../../core/layout/header/header';
 import { JoinedCarousel } from '../../components/joined-carousel/joined-carousel';
 import { CreatedCarousel } from "../../components/created-carousel/created-carousel";
 import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-actividades',
-  imports: [JoinGallery, Header, JoinedCarousel, CreatedCarousel, ButtonModule],
+  imports: [JoinGallery, Header, JoinedCarousel, CreatedCarousel, ButtonModule, RouterModule],
   templateUrl: './actividades.html',
   styleUrl: './actividades.scss'
 })
