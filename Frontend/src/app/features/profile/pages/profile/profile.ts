1e073f601dbe3dc5e2faa080ef07ce612febfb24
MarcBalague <marcbalague2004@gmail.com>
2025-12-04 15:58:01 +0100

    feat: estructura page profile creada

--- Frontend/src/app/features/profile/pages/profile/profile.ts
+++ Frontend/src/app/features/profile/pages/profile/profile.ts
@@ -2,13 +2,27 @@ import { Component } from '@angular/core';
 import { Header } from '../../../../core/layout/header/header';
 import { FormProfile } from '../../components/form-profile/form-profile';
 import { AvatarProfile } from '../../components/avatar-profile/avatar-profile';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-profile',
-  imports: [Header, FormProfile, AvatarProfile],
+  imports: [Header, FormProfile, AvatarProfile, ButtonModule],
   templateUrl: './profile.html',
   styleUrl: './profile.scss'
 })
 export class Profile {
+
+
+
+
+
+
+  eliminarCuenta() {
+    console.log("hola")
+
+  }
+
+
 
 }
