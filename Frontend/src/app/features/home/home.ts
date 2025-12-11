9860cc7476471233ef3ed0baf3957e5faddd52ae
Alex-Vilchez <alexvilchezpena@gmail.com>
2025-12-11 12:59:12 +0100

    feat: comenzar home component

--- Frontend/src/app/features/home/home.ts
+++ Frontend/src/app/features/home/home.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Header } from '../../core/layout/header/header';
 import { Footer } from '../../core/layout/footer/footer';
 import { Calendar } from './components/calendar/calendar';
+import { PerfilService } from '../../core/services/perfil/perfil-service';
+import { AvatarPipe } from '../../shared/pipes/avatar-pipe';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Footer, Calendar],
+  imports: [Header, Footer, Calendar, AvatarPipe],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
+    public perfilService = inject(PerfilService);
 
 }
