7396eb4aff7ab6552e922e0cec3132f1cf365511
MarcBalague <marcbalague2004@gmail.com>
2025-12-18 08:05:54 +0100

    feat: servico y controller notificacion terminado

--- Backend_Up2Play/backend/src/main/java/com/Up2Play/backend/Controller/NotificacionController.java
+++ Backend_Up2Play/backend/src/main/java/com/Up2Play/backend/Controller/NotificacionController.java
@@ -1,10 +1,15 @@
 package com.Up2Play.backend.Controller;
 
 import java.util.List;
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.Up2Play.backend.DTO.Respuestas.NotificacionDtoResp;
@@ -16,8 +21,6 @@ import jakarta.transaction.Transactional;
 //import java.time.LocalDateTime;
 //import com.Up2Play.backend.Repository.ActividadRepository;
 
-
-
 @RestController
 @RequestMapping("/notificaciones")
 @CrossOrigin(origins = "http://localhost:4200")
@@ -25,14 +28,6 @@ public class NotificacionController {
 
     private final NotificacionService notificacionService;
     private final UsuarioRepository usuarioRepository;
-    // private final ActividadRepository actividadRepository;
-
-/*     public NotificacionController(NotificacionService notificacionService, UsuarioRepository usuarioRepository,
-            ActividadRepository actividadRepository) {
-        this.notificacionService = notificacionService;
-        this.usuarioRepository = usuarioRepository;
-        this.actividadRepository = actividadRepository;
-    } */
 
     public NotificacionController(NotificacionService notificacionService, UsuarioRepository usuarioRepository) {
         this.notificacionService = notificacionService;
@@ -49,8 +44,63 @@ public class NotificacionController {
         return notificacionService.getNotificacionesUsuario(usuario.getId());
     }
 
+    @Transactional
+    @GetMapping("/getNotificacionesUsuario")
+    public List<NotificacionDtoResp> getNotificacionesUsuarioNoLeidas(@AuthenticationPrincipal UserDetails principal) {
+        String email = principal.getUsername();
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+
+        return notificacionService.getNotificacionesUsuario(usuario.getId());
+    }
+
+    @Transactional
+    @GetMapping("/getNotificacionesUsuario")
+    public List<NotificacionDtoResp> getNotificacionesUsuarioLeidas(@AuthenticationPrincipal UserDetails principal) {
+        String email = principal.getUsername();
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+
+        return notificacionService.getNotificacionesUsuario(usuario.getId());
+    }
+
+    @Transactional
+    @PutMapping("/leerNotificacion/{id}")
+    public ResponseEntity<?> Leer(@PathVariable Long id, @AuthenticationPrincipal UserDetails principal) {
+
+        String email = principal.getUsername();
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+
+        notificacionService.LeerNotificacion(id, usuario.getId());
+
+        return ResponseEntity.ok(Map.of("message", "Has leído una notificación"));
+    }
+
+    // Realmente se eliminar la instancia de la tabla M:N UsuarioNotificacion
+    @DeleteMapping("/eliminarNotificacion/{id}")
+    public ResponseEntity<?> eliminarNotificacion(@PathVariable Long id, @AuthenticationPrincipal UserDetails principal) {
+
+        String email = principal.getUsername();
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+
+        notificacionService.EliminarNotificacion(id, usuario.getId());
+
+        return ResponseEntity.ok(Map.of("message", "Has eliminado una notificación"));
+    }
+
     // Pruebas CRUD
 /* 
+     private final ActividadRepository actividadRepository;
+
+     public NotificacionController(NotificacionService notificacionService, UsuarioRepository usuarioRepository,
+            ActividadRepository actividadRepository) {
+        this.notificacionService = notificacionService;
+        this.usuarioRepository = usuarioRepository;
+        this.actividadRepository = actividadRepository;
+    } 
+
     @Transactional
     @PostMapping("/crearNotificacion")
     public ResponseEntity<?> crearNotificacion(@AuthenticationPrincipal UserDetails principal) {
@@ -67,14 +117,5 @@ public class NotificacionController {
         notificacionService.crearNotificacion("tiiitul", "descripciooo", LocalDateTime.now(), EstadoNotificacion.INSCRITO,
                 actividad, usuariosUnidos, usuario);
         return ResponseEntity.ok(Map.of("message", "Se ha creado una nueva notificación."));
-    }
-
-    @Transactional
-    @PutMapping("/leer")
-    public ResponseEntity<?> Leer(@AuthenticationPrincipal UserDetails principal) {
-
-        notificacionService.Leer(21L, 591L);
-
-        return ResponseEntity.ok(Map.of("message", "Se ha leído una nueva notificación."));
     } */
 }
--- Backend_Up2Play/backend/src/main/java/com/Up2Play/backend/Service/NotificacionService.java
+++ Backend_Up2Play/backend/src/main/java/com/Up2Play/backend/Service/NotificacionService.java
@@ -23,90 +23,157 @@ import jakarta.transaction.Transactional;
 @Service
 public class NotificacionService {
 
-    private UsuarioRepository usuarioRepository;
-    private NotificacionRepository notificacionRepository;
-    private UsuarioNotificacionRepository usuarioNotificacionRepository;
+        private UsuarioRepository usuarioRepository;
+        private NotificacionRepository notificacionRepository;
+        private UsuarioNotificacionRepository usuarioNotificacionRepository;
 
-    // CRUD
+        // CRUD
 
-    public NotificacionService(UsuarioRepository usuarioRepository,
-            NotificacionRepository notificacionRepository,
-            UsuarioNotificacionRepository usuarioNotificacionRepository) {
-        this.usuarioRepository = usuarioRepository;
-        this.notificacionRepository = notificacionRepository;
-        this.usuarioNotificacionRepository = usuarioNotificacionRepository;
-    }
+        public NotificacionService(UsuarioRepository usuarioRepository,
+                        NotificacionRepository notificacionRepository,
+                        UsuarioNotificacionRepository usuarioNotificacionRepository) {
+                this.usuarioRepository = usuarioRepository;
+                this.notificacionRepository = notificacionRepository;
+                this.usuarioNotificacionRepository = usuarioNotificacionRepository;
+        }
 
-    @Transactional
-    public List<NotificacionDtoResp> getNotificacionesUsuario(Long usuarioId) {
+        @Transactional
+        public List<NotificacionDtoResp> getNotificacionesUsuario(Long usuarioId) {
 
-        Usuario usuario = usuarioRepository.findById(usuarioId)
-                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+                Usuario usuario = usuarioRepository.findById(591L)
+                                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
 
-        // Crido a usuarioNotificaciones(taula M:N) per obtenir les notificacions de
-        // l'usuari
-        return usuario.getUsuarioNotificaciones().stream().map(n -> new NotificacionDtoResp(
+                // Crido a usuarioNotificaciones(taula M:N) per obtenir les notificacions de
+                // l'usuari
+                return usuario.getUsuarioNotificaciones().stream().map(n -> new NotificacionDtoResp(
 
-                n.getNotificacion().getId(),
-                n.getNotificacion().getTitulo(),
-                n.getNotificacion().getDescripcion(),
-                n.getNotificacion().getFecha() != null ? n.getNotificacion().getFecha().toString() : null,
-                n.isLeido(),
-                n.getNotificacion().getEstadoNotificacion().toString(),
-                n.getNotificacion().getActividad().getId())).toList();
+                                n.getNotificacion().getId(),
+                                n.getNotificacion().getTitulo(),
+                                n.getNotificacion().getDescripcion(),
+                                n.getNotificacion().getFecha() != null ? n.getNotificacion().getFecha().toString()
+                                                : null,
+                                n.isLeido(),
+                                n.getNotificacion().getEstadoNotificacion().toString(),
+                                n.getNotificacion().getActividad().getId())).toList();
 
-    }
+        }
 
-    @Transactional
-    public Notificacion crearNotificacion(String titulo, String descripcion, LocalDateTime fecha,
-            EstadoNotificacion estadoNotificacion, Actividad actividad, Set<Usuario> usuarios, Usuario usuarioCreador) {
+        @Transactional
+        public List<NotificacionDtoResp> getNotificacionesUsuarioNoLeidas(Long usuarioId) {
 
-        // Creo instancia de notificacion
-        Notificacion n = new Notificacion();
+                Usuario usuario = usuarioRepository.findById(usuarioId)
+                                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
 
-        n.setTitulo(titulo);
-        n.setDescripcion(descripcion);
-        n.setFecha(fecha);
-        n.setEstadoNotificacion(estadoNotificacion);
-        n.setActividad(actividad);
-        n.setUsuarioCreador(usuarioCreador);
+                // Crido a usuarioNotificaciones(taula M:N) per obtenir les notificacions de
+                // l'usuari
+                return usuario.getUsuarioNotificaciones().stream().filter(n -> !n.isLeido())
+                                .map(n -> new NotificacionDtoResp(
 
-        // La guardo
-        Notificacion notificacionGuardada = notificacionRepository.save(n);
+                                                n.getNotificacion().getId(),
+                                                n.getNotificacion().getTitulo(),
+                                                n.getNotificacion().getDescripcion(),
+                                                n.getNotificacion().getFecha() != null
+                                                                ? n.getNotificacion().getFecha().toString()
+                                                                : null,
+                                                n.isLeido(),
+                                                n.getNotificacion().getEstadoNotificacion().toString(),
+                                                n.getNotificacion().getActividad().getId()))
+                                .toList();
+        }
 
-        // Creo una lista de UsuarioNotificacion per cada usuario en la tabla M:N
-        List<UsuarioNotificacion> enlaces = usuarios.stream()
-                .map(u -> {
-                    UsuarioNotificacion un = new UsuarioNotificacion();
-                    un.setUsuario(u);
-                    un.setNotificacion(notificacionGuardada);
-                    un.setLeido(false);
-                    return un;
-                })
-                .toList();
+        @Transactional
+        public List<NotificacionDtoResp> getNotificacionesUsuarioLeidas(Long usuarioId) {
 
-        // Guardo la lista en la base de datos
-        usuarioNotificacionRepository.saveAll(enlaces);
+                Usuario usuario = usuarioRepository.findById(usuarioId)
+                                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
 
-        return notificacionGuardada;
-    }
+                // Crido a usuarioNotificaciones(taula M:N) per obtenir les notificacions de
+                // l'usuari
+                return usuario.getUsuarioNotificaciones().stream().filter(n -> n.isLeido())
+                                .map(n -> new NotificacionDtoResp(
 
-    @Transactional
-    public Boolean Leer(Long id, Long usuarioId) {
+                                                n.getNotificacion().getId(),
+                                                n.getNotificacion().getTitulo(),
+                                                n.getNotificacion().getDescripcion(),
+                                                n.getNotificacion().getFecha() != null
+                                                                ? n.getNotificacion().getFecha().toString()
+                                                                : null,
+                                                n.isLeido(),
+                                                n.getNotificacion().getEstadoNotificacion().toString(),
+                                                n.getNotificacion().getActividad().getId()))
+                                .toList();
+        }
 
-        Usuario usuario = usuarioRepository.findById(usuarioId)
-                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+        @Transactional
+        public Notificacion crearNotificacion(String titulo, String descripcion, LocalDateTime fecha,
+                        EstadoNotificacion estadoNotificacion, Actividad actividad, Set<Usuario> usuarios,
+                        Usuario usuarioCreador) {
 
-        Notificacion notificacion = notificacionRepository.findById(id)
-                .orElseThrow(() -> new ActividadNoEncontrada("Notificación no encontrada"));
+                // Creo instancia de notificacion
+                Notificacion n = new Notificacion();
 
-        UsuarioNotificacion usuarioNotificacion = usuarioNotificacionRepository.findByUsuarioAndNotificacion(usuario,
-                notificacion);
+                n.setTitulo(titulo);
+                n.setDescripcion(descripcion);
+                n.setFecha(fecha);
+                n.setEstadoNotificacion(estadoNotificacion);
+                n.setActividad(actividad);
+                n.setUsuarioCreador(usuarioCreador);
 
-        usuarioNotificacion.setLeido(true);
+                // La guardo
+                Notificacion notificacionGuardada = notificacionRepository.save(n);
 
-        usuarioNotificacionRepository.save(usuarioNotificacion);
+                // Creo una lista de UsuarioNotificacion per cada usuario en la tabla M:N
+                List<UsuarioNotificacion> enlaces = usuarios.stream()
+                                .map(u -> {
+                                        UsuarioNotificacion un = new UsuarioNotificacion();
+                                        un.setUsuario(u);
+                                        un.setNotificacion(notificacionGuardada);
+                                        un.setLeido(false);
+                                        return un;
+                                })
+                                .toList();
 
-        return true;
-    }
+                // Guardo la lista en la base de datos
+                usuarioNotificacionRepository.saveAll(enlaces);
+
+                return notificacionGuardada;
+        }
+
+        @Transactional
+        public Boolean LeerNotificacion(Long notificaionId, Long usuarioId) {
+
+                Usuario usuario = usuarioRepository.findById(usuarioId)
+                                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+
+                Notificacion notificacion = notificacionRepository.findById(notificaionId)
+                                .orElseThrow(() -> new ActividadNoEncontrada("Notificación no encontrada"));
+
+                UsuarioNotificacion usuarioNotificacion = usuarioNotificacionRepository.findByUsuarioAndNotificacion(
+                                usuario,
+                                notificacion);
+
+                usuarioNotificacion.setLeido(true);
+
+                usuarioNotificacionRepository.save(usuarioNotificacion);
+
+                return true;
+        }
+
+        @Transactional
+        public Boolean EliminarNotificacion(Long notificaionId, Long usuarioId) {
+
+                Usuario usuario = usuarioRepository.findById(usuarioId)
+                                .orElseThrow(() -> new UsuarioNoEncontradoException("Usuario no encontrado"));
+
+                Notificacion notificacion = notificacionRepository.findById(notificaionId)
+                                .orElseThrow(() -> new ActividadNoEncontrada("Notificación no encontrada"));
+
+                UsuarioNotificacion usuarioNotificacion = usuarioNotificacionRepository.findByUsuarioAndNotificacion(
+                                usuario,
+                                notificacion);
+
+                usuarioNotificacionRepository.deleteById(usuarioNotificacion.getId());
+
+                return true;
+        }
 }
