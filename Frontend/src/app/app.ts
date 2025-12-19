2d0ffef46cf9e92e5bf580dfaeecefac159a2178
MarcBalague <marcbalague2004@gmail.com>
2025-12-19 10:18:43 +0100

    feat: snow

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,17 +1,30 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 // PrimeNG
 import { Toast } from 'primeng/toast';
+import { SnowfallComponent } from './features/navidad/components/snowfall-component/snowfall-component';
+import { Header } from './core/layout/header/header';
+import { AuthHeaderComponent} from './features/auth/components/auth-header.component/auth-header.component';
+import { SnowService } from './core/services/navidad/snow-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toast],
+  imports: [RouterOutlet, Toast, SnowfallComponent, Header, AuthHeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 
 export class App {
   protected readonly title = signal('Frontend');
+
+
+  private snow = inject(SnowService);
+
+  // Reactivo: se actualiza cuando cualquier header cambia  // Reactivo: se actualiza cuando cualquier header cambia el estado
+  showSnow = computed(() => this.snow.isSnowing());
+
+
+
 
 }
