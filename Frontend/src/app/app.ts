d9aa80883f840fd46cb7a63e499debe9d36fea5e
JoJu2 <joanjuncosavilaplana@gmail.com>
2025-12-22 07:44:20 +0100

    feat: scroll top al moverte entre páginas

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,15 +1,17 @@
 import { Component, computed, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { PrimeNG } from 'primeng/config';
+import { filter } from 'rxjs';
 
 // PrimeNG
 import { Toast } from 'primeng/toast';
 import { PRIMENG_ES } from './core/i18n/primeng-es';
 import { SnowfallComponent } from './features/navidad/components/snowfall-component/snowfall-component';
 import { Header } from './core/layout/header/header';
-import { AuthHeaderComponent} from './features/auth/components/auth-header.component/auth-header.component';
+import { AuthHeaderComponent } from './features/auth/components/auth-header.component/auth-header.component';
 import { SnowService } from './core/services/navidad/snow-service';
 import { SantaComponent } from './features/navidad/components/santa-component/santa-component';
+
 
 @Component({
   selector: 'app-root',
@@ -18,13 +20,24 @@ import { SantaComponent } from './features/navidad/components/santa-component/sa
   styleUrl: './app.scss'
 })
 
-export class App implements OnInit{
+export class App implements OnInit {
   protected readonly title = signal('Frontend');
 
   private primengConfig = inject(PrimeNG)
 
+
+  constructor(private router: Router) { }
+
+
   ngOnInit(): void {
     this.primengConfig.setTranslation(PRIMENG_ES)
+
+    this.router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }); // o 'auto' scroll top al cambiar de páginas
+      });
+
   }
 
   private snow = inject(SnowService);
