4c7c48454c09fb64f625313bd47e46222627bac7
JoJu2 <joanjuncosavilaplana@gmail.com>
2026-01-08 14:11:33 +0100

    feat: estilos página pago confirmado

--- Frontend/src/app/features/pagos/pages/pago-confirmado/pago-confirmado.ts
+++ Frontend/src/app/features/pagos/pages/pago-confirmado/pago-confirmado.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from '../../../../core/layout/header/header';
+import { Footer } from '../../../../core/layout/footer/footer';
 
 @Component({
   selector: 'app-pago-confirmado',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './pago-confirmado.html',
   styleUrl: './pago-confirmado.scss',
 })
