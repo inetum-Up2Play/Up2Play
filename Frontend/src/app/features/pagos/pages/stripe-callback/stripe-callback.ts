46b7b6ca66189f5714d1b8948bb5b83beeba7506
davi2006-sudo <dasava1979@gmail.com>
2026-01-08 14:11:10 +0100

    style: añadir y cambiar contenido de la pagina html

--- Frontend/src/app/features/pagos/pages/stripe-callback/stripe-callback.ts
+++ Frontend/src/app/features/pagos/pages/stripe-callback/stripe-callback.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from '../../../../core/layout/header/header';
+import { Footer } from '../../../../core/layout/footer/footer';
 
 @Component({
   selector: 'app-stripe-callback',
-  imports: [],
+  imports: [Header,Footer],
   templateUrl: './stripe-callback.html',
   styleUrl: './stripe-callback.scss',
 })
