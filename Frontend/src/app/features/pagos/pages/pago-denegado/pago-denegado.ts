b45e663b82df3b535981bfa0be9d3b7c95b30bb3
africa-rmico <africa.rmico@gmail.com>
2026-01-09 10:46:55 +0100

    feat: retocar estilos pago denegado

--- Frontend/src/app/features/pagos/pages/pago-denegado/pago-denegado.ts
+++ Frontend/src/app/features/pagos/pages/pago-denegado/pago-denegado.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Header } from '../../../../core/layout/header/header';
+import { Footer } from '../../../../core/layout/footer/footer';
+
 
 @Component({
   selector: 'app-pago-denegado',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './pago-denegado.html',
   styleUrl: './pago-denegado.scss',
 })
 export class PagoDenegado {
+  
+  openSupport() {
+  window.open('https://support.stripe.com/?referrerLocale=es-es', '_blank', 'noopener,noreferrer');
+}
 
 }
