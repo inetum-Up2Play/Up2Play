8fe6bac3e6a2dd777cbc6230f7ac606d4d7898de
JoJu2 <joanjuncosavilaplana@gmail.com>
2026-01-09 08:43:04 +0100

    feat: links pago confirmado + estilos stripe callback

--- Frontend/src/app/features/pagos/pages/pago-confirmado/pago-confirmado.ts
+++ Frontend/src/app/features/pagos/pages/pago-confirmado/pago-confirmado.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, input } from '@angular/core';
 import { Header } from '../../../../core/layout/header/header';
 import { Footer } from '../../../../core/layout/footer/footer';
+import { Router } from '@angular/router';
+import { ActService } from '../../../../core/services/actividad/act-service';
 
 @Component({
   selector: 'app-pago-confirmado',
@@ -9,5 +11,28 @@ import { Footer } from '../../../../core/layout/footer/footer';
   styleUrl: './pago-confirmado.scss',
 })
 export class PagoConfirmado {
+  private router = inject(Router);
+  private actService = inject(ActService);
+
+  titulo = input.required<string>();
+  precio = input.required<string>();
+  fecha = input.required<string>();
+  hora = input.required<string>();
+  ubicacion = input.required<string>();
+  imagen = input<string>('');
+
+  @Input() actividadId!: number;
+
+  goToInfoActividad() {
+
+    return this.router.navigate([`/actividades/info-actividad/${this.actividadId}`]);
+
+  }
+
+  goToInicio() {
+
+    return this.router.navigate([`/`]);
+
+  }
 
 }
