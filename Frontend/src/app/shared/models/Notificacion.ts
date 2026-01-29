d3b544b5d58ecac0a05595b5a2de1373e84f6732
davi2006-sudo <dasava1979@gmail.com>
2026-01-29 14:02:54 +0100

    feature: Arreglar en notificaciones los 'barrabaja'
    feature: Investigar como añadir más colores en 'tipo de notificación'

--- Frontend/src/app/shared/models/Notificacion.ts
+++ Frontend/src/app/shared/models/Notificacion.ts
@@ -1,16 +1,26 @@
-import { Actividad } from './Actividad'; 
-import { Usuario } from './usuario.model';   
+import { Actividad } from './Actividad';
+import { Usuario } from './usuario.model';
 export interface Notificacion {
-    id: number;
-    titulo: string;
-    descripcion: string;
-    fecha: string; //'YYYY-MM-DD'
-    leido: boolean;
-    // Enum del backend mapeado a string literals para mayor seguridad
-    estadoNotificacion: 'INSCRITO' | 'PAGADO' | 'ACTUALIZADO' | 'DESAPUNTADO' | 'CANCELADA' | 'CREADA' | 'EDITADA';
-    
-    // Relaciones (opcionales por si el backend manda null en alguna)
-    actividad?: Actividad;
-    usuarios?: Usuario;
-    usuarioCreador?: Usuario;
+  id: number;
+  titulo: string;
+  descripcion: string;
+  fecha: string; //'YYYY-MM-DD'
+  leido: boolean;
+  // Enum del backend mapeado a string literals para mayor seguridad
+  estadoNotificacion:
+    | 'INSCRITO'
+    | 'PAGADO'
+    | 'ACTUALIZADO'
+    | 'DESAPUNTADO'
+    | 'CANCELADA'
+    | 'CREADA'
+    | 'EDITADA'
+    | 'REEMBOLSADO'
+    | 'PAGO_RECIBIDO'
+    | 'PAGO_FALLIDO';
+
+  // Relaciones (opcionales por si el backend manda null en alguna)
+  actividad?: Actividad;
+  usuarios?: Usuario;
+  usuarioCreador?: Usuario;
 }
